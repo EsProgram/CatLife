0d202d39ed4014259d48198a9b0b54d305d1bc78
Es_Program <radwimps.september@gmail.com>
2014-09-08 00:38:22 +0900

    add rotate aim-pointer

---
+++ Assets/Script/AimPointerRotate.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class AimPointerRotate : MonoBehaviour
+{
+    [SerializeField]
+    private float speed = 180f;
+
+    private void Update()
+    {
+        transform.Rotate(Vector3.up, speed * Time.deltaTime);
+    }
+}
